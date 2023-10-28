89bf30dd527dd947e795ddff88635f9ae074a4a1
Anthony Climer <anthonyclimer0925@gmail.com>
2023-10-27 21:36:24 -0400

    I have the left button functionality working

--- src/components/imagesSlider.jsx
+++ src/components/imagesSlider.jsx
@@ -3,14 +3,27 @@ import {ArrowBigLeft, ArrowBigRight} from 'lucide-react'
 
 
 export default function ImagesSlider(props){
-    const [imageIndex, setImageIndex] = useState(4);
-         
+    const [imageIndex, setImageIndex] = useState(0);
+     
+    function showNextImage (){
+
+    }
+
+    function showPrevImage(){
+        setImageIndex( index => {
+            if(index === 0){
+                return props.catIMAGES.length - 1
+            }
+
+            return index - 1
+        })
+    }
 
     return(
         <div style = {{width:'100%', height:'100%', position:'relative'}} >
             <img src={props.catIMAGES[imageIndex]} alt ={`cat Images ${imageIndex} `} className="img-slider-div"/>
-            <button className="img-slider-btn" style={{left:0}}><ArrowBigLeft/></button>
-            <button className="img-slider-btn" style={{right:0}}><ArrowBigRight/></button>
+            <button onClick={showPrevImage} className="img-slider-btn" style={{left:0}}><ArrowBigLeft/></button>
+            <button onClick={showNextImage} className="img-slider-btn" style={{right:0}}><ArrowBigRight/></button>
         </div>
     )
 }
